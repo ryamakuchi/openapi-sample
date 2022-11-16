736513bd767502d2bd73a22343b4944a759d05b4
rry <r.yamakuchi@gmail.com>
2022-11-17 06:29:21 +0900

    feat: UnauthorizedErrorResponse を追加

--- packages/api-client/src/generated/models/index.ts
+++ packages/api-client/src/generated/models/index.ts
@@ -1,3 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './ErrorResponse';
+export * from './UnauthorizedErrorResponse';
+export * from './UnauthorizedErrorResponseAllOf';
+export * from './UnauthorizedErrorResponseAllOfPayload';
